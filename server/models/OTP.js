8453c1cb1777212e24f14c51e84842460593692a
itsritesh02 <itsritesh02@gmail.com>
2026-08-14 17:32:51 +0530

    otp .js created

---
+++ server/models/OTP.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    otp: {
+      type: String,
+      required: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const OTP = mongoose.model("OTP", otpSchema);
+
+export default OTP;
