ebb7bc8d2abbcdf7f9e2b34c99cfe8230086abc5
itsritesh02 <itsritesh02@gmail.com>
2026-08-14 17:33:37 +0530

    authRoutes Updated

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+
+import { register, login, getProfile } from "../controllers/authController.js";
+
+import {authMiddleware,authorizeRoles} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+
 router.post("/login", login);
 
-export default router;
+router.get("/profile", authMiddleware, getProfile);
 
+router.get("/admin", authMiddleware, authorizeRoles("admin"), (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome Admin",
+  });
+});
+
+export default router;
