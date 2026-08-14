ca6fba7f6df4195f84a44543d33906bd77fec708
itsritesh02 <itsritesh02@gmail.com>
2026-08-14 17:20:37 +0530

    authController created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -113,3 +113,27 @@ export const login = async (req, res) => {
     });
   }
 };
+
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
