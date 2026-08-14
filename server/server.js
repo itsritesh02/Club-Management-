19d9fcb1c2ffe4969f688596b8249d0aa1927e93
itsritesh02 <itsritesh02@gmail.com>
2026-08-14 15:42:11 +0530

    connected MongoDb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
@@ -21,3 +24,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
