c67c105b9622592103f33c042ca6460dd43b9051
itsritesh02 <itsritesh02@gmail.com>
2026-08-17 14:39:29 +0530

    emailServices mod

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,7 +1,9 @@
+import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -12,9 +14,12 @@ export const sendOTPEmail = async (email, otp) => {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
+
     subject: "Club Management - OTP Verification",
+
     html: `
       <div style="font-family: Arial; padding: 20px;">
+
         <h2>Club Management System</h2>
 
         <p>Your OTP for email verification is:</p>
@@ -23,7 +28,11 @@ export const sendOTPEmail = async (email, otp) => {
 
         <p>This OTP will expire in 5 minutes.</p>
 
-        <p>If you did not request this OTP, please ignore this email.</p>
+        <p>
+          If you did not request this OTP,
+          please ignore this email.
+        </p>
+
       </div>
     `,
   });
